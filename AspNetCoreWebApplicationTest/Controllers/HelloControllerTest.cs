3b75a3f12eb10b1a0251c55a3192584ef1f2fc7f
Yi Tao <yitao1991@gmail.com>
2022-11-11 10:57:24 +0800

    Delete HelloControllerTest.cs

--- AspNetCoreWebApplicationTest/Controllers/HelloControllerTest.cs
+++
@@ -1,7 +0,0 @@
-        [Fact]
-        public void NoInputParamGetResponseTest()
-        {
-            HelloController controller = new HelloController();
-            var response = controller.Get().Value as Response;
-            Assert.Equal("Hello World!!!", response.output);
-        }
